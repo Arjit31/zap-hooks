a371442ddd4a490457229d90022b080ec6f63ddf
Arjit31 <apk20023110@gmail.com>
2025-11-04 22:18:01 +0530

    complete basic functionality

---
+++ prisma/migrations/20251101093327_init/migration.sql
@@ -0,0 +1,68 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Zap" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Zap_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Trigger" (
+    "id" TEXT NOT NULL,
+    "zapId" TEXT NOT NULL,
+    "typeId" TEXT NOT NULL,
+
+    CONSTRAINT "Trigger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Action" (
+    "id" TEXT NOT NULL,
+    "zapId" TEXT NOT NULL,
+    "typeId" TEXT NOT NULL,
+
+    CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AvailableTrigger" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "AvailableTrigger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AvailableAction" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "AvailableAction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Trigger_zapId_key" ON "Trigger"("zapId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Action_zapId_key" ON "Action"("zapId");
+
+-- AddForeignKey
+ALTER TABLE "Trigger" ADD CONSTRAINT "Trigger_typeId_fkey" FOREIGN KEY ("typeId") REFERENCES "AvailableTrigger"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Trigger" ADD CONSTRAINT "Trigger_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "Zap"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Action" ADD CONSTRAINT "Action_typeId_fkey" FOREIGN KEY ("typeId") REFERENCES "AvailableAction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Action" ADD CONSTRAINT "Action_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "Zap"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+app.use(express.json())
+const prisma = new PrismaClient();
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
-    const userId = req.body.userId;
-    const zapId = req.body.zapId;
-})
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    console.log(body)
+
+    await prisma.$transaction(async (tx) => {
+        const zapRun = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        })
+        await tx.zapRunOutbox.create({
+            data:{
+                zapRunId: zapRun.id
+            }
+        })
+    });
+    res.json({message: "webhook received"})
+});
+
+app.listen(3000)
