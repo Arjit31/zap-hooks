efcb2b44b32ed155789138282a616f3df62a0b98
Arjit31 <apk20023110@gmail.com>
2025-11-30 16:38:13 +0530

    upgrade prisma

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,36 +1,30 @@
 import express from "express";
-import { PrismaClient } from "./generated/prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-
-const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL 
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
 });
-
 const app = express();
-app.use(express.json())
-const prisma = new PrismaClient({adapter});
-
+app.use(express.json());
+const prisma = new PrismaClient({ adapter });
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
     const body = req.body;
-
-    console.log(body)
-
+    console.log(body);
     await prisma.$transaction(async (tx) => {
         const zapRun = await tx.zapRun.create({
             data: {
                 zapId: zapId,
                 metadata: body
             }
-        })
+        });
         await tx.zapRunOutbox.create({
-            data:{
+            data: {
                 zapRunId: zapRun.id
             }
-        })
+        });
     });
-    res.json({message: "webhook received"})
+    res.json({ message: "webhook received" });
 });
-
-app.listen(3000)
+app.listen(3000);
