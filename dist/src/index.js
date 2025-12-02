5c5ce69ed9c83547822107170af42d1cc516f5e5
Arjit31 <apk20023110@gmail.com>
2025-12-02 23:09:02 +0530

    final fix prisma

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,30 +1,44 @@
-import express from "express";
-import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-const adapter = new PrismaPg({
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const client_1 = require("./generated/prisma/client");
+const adapter_pg_1 = require("@prisma/adapter-pg");
+const adapter = new adapter_pg_1.PrismaPg({
     connectionString: process.env.DATABASE_URL
 });
-const app = express();
-app.use(express.json());
-const prisma = new PrismaClient({ adapter });
-app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const prisma = new client_1.PrismaClient({ adapter });
+app.post("/hooks/catch/:userId/:zapId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
     const body = req.body;
     console.log(body);
-    await prisma.$transaction(async (tx) => {
-        const zapRun = await tx.zapRun.create({
+    yield prisma.$transaction((tx) => __awaiter(void 0, void 0, void 0, function* () {
+        const zapRun = yield tx.zapRun.create({
             data: {
                 zapId: zapId,
                 metadata: body
             }
         });
-        await tx.zapRunOutbox.create({
+        yield tx.zapRunOutbox.create({
             data: {
                 zapRunId: zapRun.id
             }
         });
-    });
+    }));
     res.json({ message: "webhook received" });
-});
+}));
 app.listen(3000);
